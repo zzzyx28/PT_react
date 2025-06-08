1f2877dc04346830521393ce61f2dcab7fd97fdc
NailMak <1216794860@qq.com>
2025-06-08 21:19:09 +0800

    Update Navbar.jsx
    添加management，并修改了一点顶部导航栏

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,55 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+export default function Navbar({ showBackButton = false }) {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed"> {/* fixed to top */}
-        <Toolbar
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography variant="h6" sx={{ ml: 2 }}>
-            My PT Site
-          </Typography>
-          <Box sx={{ display: 'flex', gap: 2, mr: 2 }}>
-            <Button color="inherit" component={Link} to="/home">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/announcement">
-              Announcement
-            </Button>
-            <Button color="inherit" component={Link} to="/forum">
-              Forum
-            </Button>
-            <Button color="inherit" component={Link} to="/search">
-              Search
-            </Button>
-            <Button color="inherit" component={Link} to="/upload">
-              Upload
-            </Button>
-            <Button color="inherit" component={Link} to="/chat">
-              Chat
-            </Button>
-            <Button color="inherit" component={Link} to="/me">
-              Me
-            </Button>
-            <Button color="inherit" component={Link} to="/torrents">
-              TorrentList
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {/* Add top margin to push content below navbar */}
-      <Toolbar /> {/* acts as a spacer equal to AppBar height */}
-    </Box>
-  )
+    <AppBar position="fixed" sx={{ height: 64 }}>
+      <Toolbar sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        height: '100%',
+        overflow: 'hidden', // 防止内容溢出
+      }}>
+        <Typography variant="h6" sx={{ ml: 2 }}>
+          My PT Site
+        </Typography>
+        <Box sx={{ 
+          display: 'flex', 
+          gap: 2, 
+          mr: 2,
+          overflowX: 'auto', // 允许横向滚动
+          whiteSpace: 'nowrap', // 防止换行
+        }}>
+          <Button color="inherit" component={Link} to="/home">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/announcement">
+            Announcement
+          </Button>
+          <Button color="inherit" component={Link} to="/forum">
+            Forum
+          </Button>
+          <Button color="inherit" component={Link} to="/search">
+            Search
+          </Button>
+          <Button color="inherit" component={Link} to="/upload">
+            Upload
+          </Button>
+          <Button color="inherit" component={Link} to="/chat">
+            Chat
+          </Button>
+          <Button color="inherit" component={Link} to="/me">
+            Me
+          </Button>
+          <Button color="inherit" component={Link} to="/torrents">
+            TorrentList
+          </Button>
+          <Button color="inherit" component={Link} to="/management">
+            Management
+          </Button>
+          
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
